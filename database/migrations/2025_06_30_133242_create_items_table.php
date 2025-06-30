f18da2de3250ba8728b68fa72ad6e70b727b842a
jlarm <117611704+jlarm@users.noreply.github.com>
2025-06-30 08:34:59 -0500

    feat: implement Item model with migration, factory and model configuration

---
+++ database/migrations/2025_06_30_133242_create_items_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('items', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(User::class)->constrained()->cascadeOnDelete();
+            $table->string('name');
+            $table->string('store')->nullable();
+            $table->string('size')->nullable();
+            $table->string('color')->nullable();
+            $table->string('link')->nullable();
+            $table->integer('price')->nullable();
+            $table->boolean('purchased')->default(false);
+            $table->string('image')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('items');
+    }
+};
