a368fdcdbf43bed2dcd3d9761930cf57253c312c
jlarm <117611704+jlarm@users.noreply.github.com>
2025-11-07 11:14:01 -0600

    added home page

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-})->name('home');
+});
 
 Route::middleware('guest')->group(function () {
     Route::livewire('register', 'pages::auth.register')->name('register');
@@ -15,6 +15,9 @@ Route::middleware('guest')->group(function () {
 });
 
 Route::middleware('auth')->group(function () {
+
+    Route::view('home', 'home')->name('home');
+
     Route::post('logout', function () {
         Auth::logout();
         request()->session()->invalidate();
